292440667e7e8ffd8a8586e83ade3ecbe64fbfb3
Aaradhana <prajapatiaaradhana65@gmail.com>
2026-07-16 11:49:33 +0530

    Time: 27 ms (33.76%) | Memory: 25.9 MB (61.25%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen=set()
+        for num in nums:
+            if num  in seen:
+                return True
+            seen.add(num)
+        return False
+a=Solution()
+print(a.containsDuplicate([1,2,3,1]))
+print(a.containsDuplicate([1,2,3,4]))
+print(a.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
