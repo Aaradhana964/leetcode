2b404aa8a28d9d9e0b662dfcd802473050711f71
Aaradhana <prajapatiaaradhana65@gmail.com>
2026-07-18 09:53:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (79.85%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
+a=Solution()
+print(a.strStr("sadbutsad","sad"))
+print(a.strStr("leetcode","leeto"))
