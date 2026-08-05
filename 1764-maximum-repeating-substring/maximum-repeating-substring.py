e7b5afd2c59533a3b2d31aaaea0b665451e3e82d
Aaradhana <prajapatiaaradhana65@gmail.com>
2026-08-05 22:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.04%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        count = 0
+        while word * (count + 1) in sequence:
+            count += 1
+        return count
+a = Solution()
+print(a.maxRepeating("ababc", "ab"))  
+print(a.maxRepeating("ababc", "ba"))   
